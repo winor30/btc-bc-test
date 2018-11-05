06a636c9d247e46629978c7d950f1c6a8c7e1cc2
Takuma Katsumata <tk.30.r.h@gmail.com>
2018-11-05 17:33:58 +0900

    generate double hash & reverse func

--- markle.go
+++ markle.go
@@ -4,6 +4,7 @@ import (
 	"encoding/hex"
 	"fmt"
 	"github.com/btcsuite/btcd/chaincfg/chainhash"
+	"strconv"
 )
 
 func fillZero(bit string) string {
@@ -24,15 +25,61 @@ func toBitString(bytes []byte) string {
 	return bits
 }
 
-func toHash(tx string) chainhash.Hash {
-	return chainhash.DoubleHashH(nil)
+func reverseBit(bit string) string {
+	newBit := ""
+	for _, c := range bit {
+		if c == '0' {
+			newBit = newBit + "1"
+		} else if c == '1' {
+			newBit = newBit + "0"
+		}
+	}
+	return newBit
+}
+
+func reverse(s string) string {
+	runes := []rune(s)
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+	return string(runes)
+}
+
+func bitToBytes(bit string) []byte {
+	var bytes []byte
+	runes := []rune(bit)
+	for i := 0; i < len(runes)/8; i++ {
+		s := string(runes[i*8 : (i+1)*8])
+		num, _ := strconv.ParseUint(s, 2, 8)
+		bytes = append(bytes, byte(num))
+	}
+	return bytes
+}
+
+func toHash(txHex string) chainhash.Hash {
+	txByte, _ := hex.DecodeString(txHex)
+	txBit := toBitString(txByte)
+	// fmt.Println(txHex, len(txHex))
+	// fmt.Println(txByte, len(txByte))
+	// fmt.Println(txBit, len(txBit))
+
+	txBit = reverseBit(txBit)
+	txBit = reverse(txBit)
+	// fmt.Println(txBit)
+
+	bytes := bitToBytes(txBit)
+
+	return chainhash.DoubleHashH(bytes)
 }
 
 func main() {
-	txStr := "4eb8629ffb3bdf1035951d6df78fdb0bf5770a1b6b5744995ad593a52b8c2dc3"
-	fmt.Println(txStr, len(txStr))
-	tx, _ := hex.DecodeString(txStr)
-	fmt.Println(tx, len(tx))
-	txBit := toBitString(tx)
-	fmt.Println(txBit, len(txBit))
+	txHex1 := "4eb8629ffb3bdf1035951d6df78fdb0bf5770a1b6b5744995ad593a52b8c2dc3"
+	txHex2 := "ba219c854e7a05df49d45e9c97bd006f3ef9daefe93a43a82c82cf0eef02569d"
+	hash1 := toHash(txHex1)
+	hash2 := toHash(txHex2)
+	fmt.Println(hash1)
+	fmt.Println(hash2)
+
+	hashSum := toHash(txHex1 + txHex2)
+	fmt.Println(hashSum)
 }
