3218a41ad42d32cf22a8864572783a2afad300e2
Takuma Katsumata <tk.30.r.h@gmail.com>
2018-11-07 08:14:06 +0900

    add validation by using markle path

--- markle.go
+++ markle.go
@@ -59,21 +59,18 @@ func bitToBytes(bit string) []byte {
 func toHash(txHex string) chainhash.Hash {
 	txByte, _ := hex.DecodeString(txHex)
 	txBit := toBitString(txByte)
-	// fmt.Println(txHex, len(txHex))
-	// fmt.Println(txByte, len(txByte))
-	// fmt.Println(txBit, len(txBit))
-
 	txBit = reverseBit(txBit)
 	txBit = reverse(txBit)
-	// fmt.Println(txBit)
-
 	bytes := bitToBytes(txBit)
-
 	return chainhash.DoubleHashH(bytes)
 }
 
-func toSumHash(txs []chainhash.Hash) []chainhash.Hash {
-	fmt.Println(txs)
+func calcMarkleRoot(txs []chainhash.Hash, txTree *[][]chainhash.Hash) []chainhash.Hash {
+	if len(txs) != 1 && len(txs)%2 == 1 {
+		txs = append(txs, txs[len(txs)-1])
+	}
+	*txTree = append(*txTree, txs)
+
 	if len(txs) == 1 {
 		return txs
 	}
@@ -86,24 +83,71 @@ func toSumHash(txs []chainhash.Hash) []chainhash.Hash {
 			smtxs = append(smtxs, toHash(txs[2*i].String()+txs[2*i].String()))
 		}
 	}
-	return toSumHash(smtxs)
+	return calcMarkleRoot(smtxs, txTree)
+}
+
+type HashPath struct {
+	ID   int
+	Hash chainhash.Hash
+}
+
+func (hash HashPath) String() string {
+
+	return strconv.Itoa(hash.ID) + ": " + hash.Hash.String()
+}
+
+func getRelatedTxs(txid int, txTree *[][]chainhash.Hash) []HashPath {
+	var marklePath []HashPath
+	for _, txs := range *txTree {
+		if txid%2 == 1 && len(txs) != 1 {
+			marklePath = append(marklePath, HashPath{ID: txid - 1, Hash: txs[txid-1]})
+		} else if txid%2 == 0 && len(txs) != 1 {
+			marklePath = append(marklePath, HashPath{ID: txid + 1, Hash: txs[txid+1]})
+		}
+
+		txid = txid / 2
+	}
+	return marklePath
+}
+
+func validation(tx chainhash.Hash, marklePath []HashPath, markleRoot chainhash.Hash) bool {
+	expectedRoot := tx
+	for _, mhash := range marklePath {
+		if mhash.ID%2 == 1 {
+			expectedRoot = toHash(expectedRoot.String() + mhash.Hash.String())
+		} else {
+			expectedRoot = toHash(mhash.Hash.String() + expectedRoot.String())
+		}
+	}
+
+	return markleRoot.String() == expectedRoot.String()
 }
 
 func main() {
-	fmt.Println(`"aaa", "bbb", "ccc", "ddd", "eee", "fff", "ggg", "hhh"`)
-	txs := []string{"aaa", "bbb", "ccc", "ddd", "eee", "fff", "ggg", "hhh"}
+	fmt.Println(`"ccc", "ddd", "aaa", "bbb", "eee"`)
+	txs := []string{"ccc", "ddd", "aaa", "bbb", "eee"}
+
 	var txHashs []chainhash.Hash
 	for _, tx := range txs {
 		txHashs = append(txHashs, toHash(tx))
 	}
-	fmt.Println(toSumHash(txHashs))
-
-	fmt.Println(`"ccc", "ddd", "aaa", "bbb", "eee"`)
-	txs = []string{"ccc", "ddd", "aaa", "bbb", "eee"}
-	txHashs = []chainhash.Hash{}
-	for _, tx := range txs {
-		txHashs = append(txHashs, toHash(tx))
+	var txTree [][]chainhash.Hash
+	markleRoot := calcMarkleRoot(txHashs, &txTree)[0]
+	fmt.Println("Markle Root is ", markleRoot)
+	fmt.Println("Markle Path is ...")
+	for i, txs := range txTree {
+		fmt.Println(i, len(txs), txs)
 	}
 
-	fmt.Println(toSumHash(txHashs))
+	// check valid transaction "aaa" that number of tx is 2
+	// it will be true
+	marklePath := getRelatedTxs(3, &txTree)
+	fmt.Println(marklePath)
+	fmt.Println("validation is ", validation(txHashs[3], marklePath, markleRoot))
+
+	// check valid transaction "aaa" that number of tx is 3
+	// it will be false
+	marklePath = getRelatedTxs(1, &txTree)
+	fmt.Println(marklePath)
+	fmt.Println("validation is ", validation(txHashs[0], marklePath, markleRoot))
 }
